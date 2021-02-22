4958916c399f772babef52035e7abd6775dec0c5
Berend <Berendcornelis@gmail.com>
2021-02-22 13:26:32 +0100

    Initial Commit

---
+++ Shared/HunkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HunkApp.swift
+//  Shared
+//
+//  Created by Berend de Ruiter on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HunkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
